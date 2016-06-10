ac3787d004533fa8cff7fb0f6102a0c242384a75
Andrii <andrii@latitude.(none)>
2016-06-10 08:05:48 +0300

    Add scroll

--- static/custom/cap.js
+++ static/custom/cap.js
@@ -65,7 +65,7 @@ app.controller('ArticleController',function($scope)
 	$('.article_img').ready(function()
 		{
 		var d = document.getElementById('scene');
-		Par(window,document);
+	//	Par(window,document);
 		$scope.parallax = new Parallax(d);
 		})
 
@@ -109,7 +109,7 @@ app.controller('ArticleController',function($scope)
 	$window.scroll(function()
 		{
 			
-		//obtain real left_col position	
+/*		//obtain real left_col position	
 		var left_col = $('.article_container .left_col');
 
 		if (left_col.length==0)
@@ -170,7 +170,7 @@ app.controller('ArticleController',function($scope)
 			$scope.cur_menu = tmp_dict[sort_dict[0]];
 			}
 		$scope.$apply();
-
+*/
 		})
 
 	$(window).resize(function()
@@ -209,6 +209,140 @@ app.controller('ArticleController',function($scope)
 	})
 
 
+app.service('decorators',function($window)
+	{
+	$window.timings = {};
+
+
+	function create_funct_obj()
+		{
+		return{
+			storage:[],
+			get count()
+			   {
+			   return this.storage.length;
+			   },
+			get duration()
+			   {
+			   var total = 0;
+			   this.storage.forEach(function(el)
+				{
+				total = total+el;
+				})
+			   return total/this.count;
+			   },
+			get max()
+			   {
+			   return Math.max.apply(null,this.storage);
+
+			   }
+		    };  
+
+		}
+
+	
+	//registration (create key for dictionary and decorate function)
+	this.registr = function(scope,name)
+		{
+		//we need to check existence of this section, because we can reuse directive in diferent views
+		
+		if ($window.timings[name]===undefined)
+			{
+			$window.timings[name] = {};  //create for new module section in timings
+			$window.timings.print = function()
+				{
+				for (var each in $window.timings)
+					{
+					console.log('----'+each+'------');
+					for (var fn_name in $window.timings[each])
+						{
+						console.log(fn_name+': #'+$window.timings[each][fn_name]['count']+', DURATION '+$window.timings[each][fn_name]['duration']+', MAX '+this[each][fn_name].max);
+						}
+					}
+				}
+			}
+
+		for (var each in scope)
+			{
+			if (typeof(scope[each])==='function')
+				{
+				if (each.indexOf('$')==-1)
+					{
+					function factory(scope,name,each)
+					   {
+					   //create function array
+				 	   if ($window.timings[name][each]===undefined)
+						{
+
+						//CREATE OBJECT
+						$window.timings[name][each] = create_funct_obj()
+						}
+
+
+					   
+					   var fn = scope[each];
+
+					   scope[each] = function(each,name,fn)
+						{
+						return function()
+							{
+							var ky = name+":"+each;
+							$window.performance.mark('mark:'+ky);
+							fn.apply(null,arguments);
+							$window.performance.measure(ky,'mark:'+ky);
+							var obj = $window.performance.getEntriesByName(ky)[0];
+							$window.timings[name][each]['storage'].push(obj['duration']);
+							if ($window.timings[name][each]['storage'].length>100000)
+								{
+								$window.timings[name][each]['storage'] = $window.timings[name][each]['storage'].splice(50000,99000);
+								}
+
+							$window.performance.clearMarks('mark:'+ky);
+							$window.performance.clearMeasures(ky);
+							}
+						}(each,name,fn);
+					};
+					factory(scope,name,each);
+				      }
+			        }
+		 	} //close for each in scope
+		}  //close registr
+
+
+	this.start = function(section_name,function_name)
+		{
+		var ky = section_name+':'+function_name;
+		$window.timings[section_name] = $window.timings[section_name]===undefined?{}:$window.timings[section_name];
+		if ($window.timings[section_name][function_name]===undefined)
+			{
+			$window.timings[section_name][function_name] = create_funct_obj();
+			}
+
+		$window.performance.mark('mark:'+ky);	
+		}
+
+	this.stop = function(section_name,function_name)
+		{
+		var ky = section_name+':'+function_name;
+		$window.performance.measure(ky,'mark:'+ky);
+		var obj = $window.performance.getEntriesByName(ky)[0];
+		$window.timings[section_name][function_name]['storage'].push(obj['duration']);
+	
+		//because of error 'maximum exceed'	
+		if ($window.timings[section_name][function_name]['storage'].length>100000)
+			{
+			$window.timings[section_name][function_name]['storage'] = $window.timings[section_name][function_name]['storage'].splice(50000,99000);
+			}
+
+		$window.performance.clearMarks('mark:'+ky);
+		$window.performance.clearMeasures(ky);
+		}
+
+	
+
+
+	})  //close decorators
+
 app.controller('ContactController',function($scope)
 	{
 	$(window).scrollTop(0); //after loading page little scrolled
@@ -220,24 +354,25 @@ app.controller('ContactController',function($scope)
 
 
 
-app.directive('parallax',function($window)
+app.directive('parallax',function($window,$timeout,decorators,$rootScope)
 	{
 	return {
 		scope:{},
 		link:function(scope,element,attr)
 			{
-			console.log('Srabotalo');
 			scope.init = init;
 			scope.getDimensions = getDimensions;
 			scope.onScroll = onScroll;
-
+			
+			decorators.registr(scope,'parallax');
+	
 
 			scope.viewportHeight = 0;
 
 			
 			scope.init();
 
-			$($window).on('scroll',scope.onScroll);
+			$(window).on('scroll',scope.onScroll);
 
 			function init()
 				{
@@ -255,11 +390,14 @@ app.directive('parallax',function($window)
 				var scale = scope.parentElementHeight/element[0].getBoundingClientRect()['height']; //determien scale koef to recalc velocity, because translate3d shift elem relative to its height but parent
 				scope.velocityY = scope.velocityY*scale;
 				
+				scope.prev_scroll_dist = 0;
+				scope.anim_run = false;
 				}
 
-
-			function onScroll()
+			function onScroll(e)
 				{
+console.log('cdcd');
+				if (!e){e = window.event}
 				//get parent element
 				var top_pos = element[0].parentElement.getBoundingClientRect()['top'];
 				//determine viewport location
@@ -271,13 +409,17 @@ app.directive('parallax',function($window)
 					{
 					return false;
 					}
+				//if (element[0].className.indexOf('bgimg')!=-1){console.log(element)};
 
 				var distCurY = top_pos+scope.parentElementHeight;
 				var relative = parseInt(distCurY/scope.distY*100);
-				console.log(relative,distCurY,scope.distY,scope.velocityY,element);
 				relative = (1-relative/100)*scope.velocityY;
-				element.css({'transform':'translate3d(0%,-'+relative+'%,0)'});
-		
+				
+				window.requestAnimationFrame(function()
+					{
+					element.css({'transform':'translate3d(0%,-'+relative+'%,0)'});
+					})
+
 				}
 
 			
@@ -288,6 +430,178 @@ app.directive('parallax',function($window)
 	}
 	
 	})
+
+
+
+
+
+
+
+
+app.directive('scroll',function($window,$timeout,decorators,$rootScope)
+	{
+	return {
+		scope:{},
+		link:function(scope,element,attr)
+			{
+			scope.init = init;
+			scope.onScroll = onScroll;
+
+
+
+			scope.DURATION = 500;
+			scope.MAX_DURATION = 1000;
+			scope.MAX_EVENTS = 30;
+			scope.TRASHHOLD = 10000;
+			scope.SCROLL_DECREASE = 2;
+			scope.LAST_DURATION = 3000;  //duration for last event;		
+	
+			decorators.registr(scope,'scroll');
+			
+			scope.init();
+			
+
+			function init()
+				{
+				$(window).on('mousewheel MozMousePixelScroll',scope.onScroll);
+				scope.events = [];
+				scope.anim_run = false;
+				}
+
+
+			function ease_quad_out(t,b,c,d)
+				{
+				t /= d;
+				return -c * t*(t-2) + b;
+				}
+
+		
+			function ease_quadratic(t,b,c,d)
+				{
+				t /= d;
+				t--;
+				return c*(t*t*t*t*t + 1) + b;
+				}
+
+			function linear(t,b,c,d)
+				{
+				return c*t/d + b;
+				}
+
+			scope.anim_run = false;
+			scope.last_event = false;
+			function onScroll(e)
+				{
+				if (!e){e = window.event}
+				e.preventDefault();
+				//for mozilla
+				if (e['originalEvent']['deltaY']===undefined)
+					{
+					e['originalEvent']['deltaY'] = e['originalEvent']['detail'];
+					}
+				var kolvo_e = scope.events.length;
+				//get last value to obtain the sign (means direction)
+				if (kolvo_e!=0)
+					{
+					var dif_ts = -scope.events[kolvo_e-1]['StartTime'] + e['timeStamp'];
+					}
+				else
+					{
+					var dif_ts = 0;
+					}
+				//var dif = e['originalEvent']['deltaY'];
+				var dif = e['originalEvent']['deltaY'];
+				dif = dif<0?-50:50;  //to fix constant move distance
+				if (dif_ts>scope.TRASHHOLD)
+					{
+					scope.events = [];
+					scope.kolvo_draw = 0;
+					scope.anim_run = false;
+					scope.last_event = false;
+					window.cancelAnimationFrame(scope.anim_run);
+					}
+				
+				//check on direction changes
+				var sign_minus = scope.events.filter(function(el){return el['Distance']<0});
+				var sign_plus = scope.events.filter(function(el){return el['Distance']>0});
+				if (dif>0&&sign_minus.length>0)
+					{
+					window.cancelAnimationFrame(scope.anim_run);
+					scope.anim_run = false;
+					scope.events = [];
+					}
+
+				if (dif<0&&sign_plus.length>0)
+					{
+					window.cancelAnimationFrame(scope.anim_run);
+					scope.anim_run = false;
+					scope.events = [];
+					}
+		
+				
+				var evt = {};
+				evt['Distance'] = dif*scope.SCROLL_DECREASE;
+				evt['StartTime'] = (new Date()).getTime(); 
+				evt['PrevDistance'] = 0;
+				kolvo_e = kolvo_e>scope.MAX_EVENTS?scope.MAX_EVENTS:kolvo_e;
+				//console.log(kolvo_e,'kolvo_e');
+				evt['Duration'] = ease_quad_out(kolvo_e,scope.DURATION,scope.MAX_DURATION,scope.MAX_EVENTS);
+				scope.events.push(evt);
+				function draw(ts)
+					{
+					scope.prev_num = scope.prev_num===undefined?0:scope.prev_num;	
+					scope.cur_num = parseInt(((new Date()).getTime())*1000/60);
+					if (scope.cur_num===scope.prev_num){return false};
+					scope.prev_num = scope.cur_num;
+					//console.log(scope.cur_num);
+					if (scope.anim_run==false){return false;}
+					scope.anim_run = window.requestAnimationFrame(draw);
+					decorators.start('scroll','draw');
+					var total_scroll = 0;
+					var cur_ts = (new Date()).getTime();
+
+					scope.events = scope.events.filter(function(el)
+						{
+						var delta = cur_ts - el['StartTime']-el['Duration'];
+						return delta<0;
+						})
+
+					if (scope.events.length==0)
+						{
+						window.cancelAnimationFrame(scope.anim_run);
+						scope.anim_run = false;
+						return false;
+						}
+
+					scope.events = scope.events.map(function(el)
+						{
+						
+						var time_pos = cur_ts - el['StartTime'];
+					//	console.log(time_pos,cur_ts,cur_ts - scope.prev_ts);
+						scope.prev_ts = cur_ts;
+					//	var position = ease_quadratic(time_pos,0,el['Distance'],el['Duration']);			
+						var position = linear(time_pos,0,el['Distance'],el['Duration']);			
+						var scroll_dist = position-el['PrevDistance'];
+						total_scroll = total_scroll+scroll_dist;
+						el['PrevDistance'] = position;
+						return el;
+						})
+					window.scrollBy(0,total_scroll);
+
+					}  //close draw function
+				scope.anim_run = window.requestAnimationFrame(draw);
+				}  //close scroll
+
+		
+			}
+
+	
+
+	}
+	
+	})
+
+
 
 
 
@@ -382,7 +696,6 @@ app.directive('animateScroll',function($window)
 				{
 				scope.elemTopPos = element[0].getBoundingClientRect()['top'];
 				scope.viewportHeight = $window.innerHeight;
-console.log(scope.elemTopPos)
 				}
 
 			function init()
